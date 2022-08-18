85a03d0b08cdc24f68543c14e4e8aef6489b68d1
SpiralP <spiral.palmer@gmail.com>
2022-08-17 19:53:39 -0700

    make wasm smaller

--- web/src/components/WasmLoading.tsx
+++ web/src/components/WasmLoading.tsx
@@ -1,15 +1,9 @@
 import { Callout, Intent, Spinner, SpinnerSize } from "@blueprintjs/core";
 import React from "react";
-import { useMount } from "react-use";
 import WasmLoaded from "/components/WasmLoaded";
-import useToaster from "/hooks/useToaster";
 import { useWasmStateInfo, WasmContext, WasmState } from "/hooks/useWasm";
 
 export default function WasmLoading() {
-  const toaster = useToaster();
-  useMount(() => {
-    toaster.show({ message: "bap" });
-  });
   const wasmStateInfo = useWasmStateInfo();
 
   switch (wasmStateInfo.state) {
