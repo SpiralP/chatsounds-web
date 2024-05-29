386e897869bb5f39841b3bd3f7eddd738cade56c
SpiralP <spiral.palmer@gmail.com>
2024-05-29 03:25:44 -0700

    import styles locally

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,4 +1,7 @@
 import { FocusStyleManager } from "@blueprintjs/core";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "normalize.css/normalize.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "/components/App";
